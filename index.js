4c840a98fe8a17084bfadbe7732886afeda3e9c8
yuff <497158804@qq.com>
2019-01-18 17:37:12 +0800

    首次提交

---
+++ index.js
@@ -0,0 +1,98 @@
+const settings = require('electron-settings')
+const path = require('path')
+const ipcRenderer = require('electron').ipcRenderer
+const _ = require('lodash')
+
+// 获取驱动
+async function getMediaSteam(isOpenAudio) {
+  let x = await navigator.mediaDevices.enumerateDevices()
+  let facerigDevice = _.find(x, {
+    label: 'FaceRig Virtual Camera'
+  })
+  if (!facerigDevice) {
+    throw '未找到facerig虚拟摄像头设备,确保facerig广播已打开(启动时的boardcast选项)'
+  }
+  let audioId = isOpenAudio ? {
+    deviceId: 'default'
+  } : false
+  let option = {
+    audio: audioId,
+    video: {
+      deviceId: facerigDevice.deviceId
+    }
+  };
+
+  let mediaStream = await navigator.mediaDevices.getUserMedia(option)
+  return mediaStream
+}
+
+
+
+//启动video
+function startVideo(mediaStream) {
+  return new Promise(resolve => {
+    var video = document.querySelector('video');
+    video.onloadedmetadata = function (e) {
+      video.play();
+      resolve()
+    };
+    video.src = window.URL.createObjectURL(mediaStream);
+  })
+}
+
+//绘制一帧
+function drawOneFrame() {
+
+  ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+  var imgdata = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  //反相处理
+  for (var i = 0; i < imgdata.height; ++i) {
+    for (var j = 0; j < imgdata.width; ++j) {
+      var x = i * 4 * imgdata.width + 4 * j,
+        r = imgdata.data[x],
+        g = imgdata.data[x + 1],
+        b = imgdata.data[x + 2]
+      let offset = Math.pow(3*(r), 2) + Math.pow(4*(g - 216), 2) + Math.pow(2*(b), 2)
+      // let offset = Math.abs(r - green[0]) + Math.abs(g - green[1]) + Math.abs(b - green[2])
+      if (offset < maxOffset) {
+        imgdata.data[x + 3] = 0
+      }
+      //图片反相：
+    }
+  }
+  ctx.putImageData(imgdata, 0, 0);
+}
+
+
+function drawCanvas() {
+  window.video = document.querySelector('video')
+  window.canvas = document.querySelector('canvas');
+  canvas.height = video.videoHeight
+  canvas.width = video.videoWidth
+  window.ctx = canvas.getContext('2d');
+  window.maxOffset = 150000
+  window.green = [0, 216, 0]
+  setInterval(drawOneFrame, 16.6);
+}
+
+
+
+async function start() {
+  //1. 获取驱动
+  let mediaStream = await getMediaSteam(settings.get('isOpenAudio'))
+  await startVideo(mediaStream)
+  // 启动video
+  drawCanvas()
+}
+
+//重载
+function restart() {
+  clearInterval(drawOneFrame)
+  start()
+}
+
+ipcRenderer.on('restart', () => {
+  restart()
+})
+
+start()
---
+++ main.js
@@ -0,0 +1,105 @@
+const path = require('path')
+const settings = require('electron-settings')
+const electron = require('electron');
+//自动更新
+var win;
+var tray; //TODO
+//初始化默认应用窗口菜单快捷键
+function initDefMenu() {
+    let menu = new electron.Menu();
+    /* f11 打开控制台 */
+    let f11_menuItem = new electron.MenuItem({
+        accelerator: 'f11',
+        click: function (menuItem, browserWindow, event) {
+            let webContents = browserWindow.webContents;
+            if (webContents != null) {
+                if (webContents.isDevToolsOpened()) {
+                    webContents.closeDevTools();
+                } else {
+                    webContents.openDevTools({
+                        mode: 'bottom'
+                    })
+                }
+            }
+        },
+        visible: false
+    });
+    /* f5:刷新页面 */
+    let f5_menuItem = new electron.MenuItem({
+        accelerator: 'f5',
+        click: function (menuItem, browserWindow, event) {
+            let webContents = browserWindow.webContents;
+            webContents.reload();
+        },
+        visible: false
+    });
+    menu.append(f11_menuItem)
+    menu.append(f5_menuItem)
+    electron.app.setApplicationMenu(menu)
+}
+
+//初始化窗口
+function initWindow() {
+    /* 打开主窗口 */
+    win = new electron.BrowserWindow({
+        transparent: true,
+        alwaysOnTop: true,
+        fullscreen: true,
+        center: true,
+        frame: false,
+        icon: path.join(__dirname, "favicon.png"),
+        title: electron.app.getName(),
+        autoHideMenuBar: true
+    });
+    win.setIgnoreMouseEvents(true)
+    win.loadFile('index.html');
+    win.on('closed', function () {
+        electron.app.quit();
+    })
+    win.on('restore',function(){
+        win.maximize()
+    })
+    // win.webContents.openDevTools()
+}
+
+//初始化托盘图标
+function initTray() {
+    tray = new electron.Tray(path.join(__dirname, "favicon.png"));
+    let quit = new electron.MenuItem({
+        label: '退出',
+        type: 'normal',
+        click: function () {
+            electron.app.quit();
+        }
+    });
+    /* f5:刷新页面 */
+    let microphone = new electron.MenuItem({
+        type: 'checkbox',
+        label: '使用麦克风',
+        checked: settings.get('isOpenAudio'),
+        click: function (item) {
+            settings.set('isOpenAudio', item.checked)
+            win.webContents.send('restart')
+        },
+    });
+    let restart = new electron.MenuItem({
+        label: '重载',
+        type: 'normal',
+        click: function () {
+            win.webContents.send('restart')
+        }
+    });
+    let menu = new electron.Menu()
+    menu.append(microphone)
+    menu.append(restart)
+    menu.append(quit)
+
+    tray.setContextMenu(menu)
+}
+
+
+electron.app.on('ready', function () {
+    initDefMenu()
+    initWindow()
+    initTray()
+});
